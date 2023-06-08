a3d230afebdc18bf8f10ddd917c5ad2cd00e2edb
nithyasonu15@gmail.com <nithyasonu15@gmail.com>
2023-06-08 10:57:12 +0530

    Dice roller app which seems to be fun while doing it.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,7 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() {
-  return runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.pink,
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('DICE'),
-          backgroundColor: Colors.pink[600],
-        ),
-        body: DicePage(),
-      ),
-    ),
-  );
-}
+void main() => runApp(DicePage());
 
 class DicePage extends StatefulWidget {
   @override
@@ -27,7 +12,7 @@ class _DicePageState extends State<DicePage> {
   var leftDice = 1;
   var rightDice = 1;
 
-  void randomNum() {
+  void roleDice() {
     setState(() {
       leftDice = Random().nextInt(6) + 1;
       rightDice = Random().nextInt(6) + 1;
@@ -36,35 +21,51 @@ class _DicePageState extends State<DicePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            child: ElevatedButton(
-              style: const ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll(Colors.pink),
-                elevation: MaterialStatePropertyAll(0),
-              ),
-              onPressed: () {
-                randomNum();
-              },
-              child: Image.asset('images/dice$leftDice.png'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.pink,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'DICE',
+            style: TextStyle(
+              fontFamily: 'Courgette',
             ),
           ),
-          const SizedBox(width: 10.0),
-          Expanded(
-            child: ElevatedButton(
-              style: const ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll(Colors.pink),
-                elevation: MaterialStatePropertyAll(0),
+          backgroundColor: Colors.pink[600],
+        ),
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.pink),
+                    elevation: MaterialStatePropertyAll(0),
+                  ),
+                  onPressed: () {
+                    roleDice();
+                  },
+                  child: Image.asset('images/dice$leftDice.png'),
+                ),
               ),
-              onPressed: () {
-                randomNum();
-              },
-              child: Image.asset('images/dice$rightDice.png'),
-            ),
+              const SizedBox(width: 10.0),
+              Expanded(
+                child: ElevatedButton(
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.pink),
+                    elevation: MaterialStatePropertyAll(0),
+                  ),
+                  onPressed: () {
+                    roleDice();
+                  },
+                  child: Image.asset('images/dice$rightDice.png'),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
